5c6238f3f1a48b66ecee268f527480e978009b2d
omarsh9 <osherif889@gmail.com>
2023-07-20 09:45:18 -0700

    C MORE FUNCTION

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,40 +1,57 @@
 #include <stdio.h>
 /**
- * long_prime_factor - find and print the largest prime factor
+ * _sqroot - find the square root
  *
- * @num: given input
+ * @z: input number
  *
- * Return: 0 always
+ * Return: square root of x
  */
-long int long_prime_factor(long int num)
+double _sqroot(double z)
 {
-	long largest = 0;
-	long int i;
+	float x, y;
+
+	x = z / 2;
+	y = 0;
+
+	while (x != y)
+	{
+		y = x;
+		x = (z / y + y) / 2;
+	}
+	return (x);
+}
+/**
+ * largest_prime_factor - find and print the largest prime factor of num
+ *
+ * @num: input number
+ *
+ * Return: largest
+ */
+void largest_prime_factor(long int num)
+{
+	int i, largest;
 
 	while (num % 2 == 0)
-	{
-		largest = 2;
-		num /= 2;
-	}
-	for (i = 3; i * i <= num; i += 2)
+		num = num / 2;
+	for (i = 3; i <= _sqroot(num); i += 2)
 	{
 		while (num % i == 0)
 		{
+			num = num / i;
 			largest = i;
-			num /= i;
 		}
 	}
 	if (num > 2)
-	{
 		largest = num;
-	}
-	return (largest);
+	printf("%d\n", largest);
 }
+/**
+ * main- entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
-	long int number = 612852475143;
-	long int result = long_prime_factor(number);
-
-	printf("%ld\n", result);
+	largest_prime_factor(612852475143);
 	return (0);
 }
