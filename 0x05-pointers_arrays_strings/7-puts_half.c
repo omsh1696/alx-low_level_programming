35ef065fd71d3872deba0ec00cc77ae7f0ca1a0b
omarsh9 <osherif889@gmail.com>
2023-07-24 07:00:25 -0700

    C pointer

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * puts_half - print the half of string
+ *
+ * @str: input string
+ *
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+		;
+
+	i++;
+	for (i /= 2; srt[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
