0e4ccf80ebf406d47d77ae8cf698fb9a6c4a65e4
omarsh9 <osherif889@gmail.com>
2023-07-27 03:20:20 -0700

    C pointer & array & string :):)

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * rot13 - encoder rot13
+ * @s: pointer to string (input data)
+ *
+ * Return: *s
+ */
+
+char *rot13(char *s)
+{
+	int i;
+	int j;
+	char alpha1[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char alpha2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (s[i] == alpha1[j])
+			{
+				s[i] = alpha2[j];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
