752736c6aa56b891e61292de5a7e8a0f27810362
omarsh9 <osherif889@gmail.com>
2023-08-04 04:08:48 -0700

    argv and argc :):)

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 /**
- * main - print the name 
+ * main - print the name
  *
  * @argc: int
  * @argv: list
  *
  * Return: 0
  */
-int main(int argc ,char **argv)
+int main(int argc, char **argv)
 {
 	(void)argc;
 	printf("%s\n", argv[0]);
