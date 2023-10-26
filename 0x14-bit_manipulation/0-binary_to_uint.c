697d85d434e9e23c6ed750670bd7418ac2515a0d
omsh1696 <om.sh1696@gmail.com>
2023-10-26 14:10:26 -0700

    bit operator :):)

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,15 +9,17 @@
 unsigned int binary_to_uint(const char *b)
 {
 	int i;
-	unsigned int value = 0;
+	unsigned int val = 0;
 
 	if (!b)
 		return (0);
+
 	for (i = 0; b[i]; i++)
 	{
-		if (b[i] != '0' && b[i] != '1')
+		if (b[i] < '0' || b[i] > '1')
 			return (0);
-		value = value * 2 + (b[i] - '0');
+		val = 2 * val + (b[i] - '0');
 	}
-	return (value);
+
+	return (val);
 }
