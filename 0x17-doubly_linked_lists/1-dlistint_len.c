9943da56845963c6750002ca85a29c6f9e84d931
omsh1696 <om.sh1696@gmail.com>
2023-12-07 07:51:50 -0800

    doubly linked list :):)

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * dlistint_len - to find the length of the linked list
+ * @h: pointer to the head of the list
+ * Return: the number
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t x;
+
+	for (x = 0; h != NULL; x++)
+		h = h->next;
+
+	return (x);
+}
